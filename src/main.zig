db0b6ae4962eea7893579365c5caa3088ac81d90
Zack Kollar <rallokkcaz@gmail.com>
2023-11-15 04:46:51 -0500

    :books: Docs, docs, docs, docs, docs, dox

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,7 @@ var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 var gpa_allocator = gpa.allocator();
 
 pub fn main() !void {
+    // Setup the systems
     try renderer.init(gpa_allocator);
     defer renderer.deinit();
 
@@ -18,19 +19,24 @@ pub fn main() !void {
     try ui.init(gpa_allocator);
     defer ui.deinit();
 
+    // Create some application state
     var mouse_position: Vec2i = .{ .x = 0, .y = 0 };
     var clear_color = .{ .r = 0, .g = 0, .b = 0 };
     var running = true;
     var current_frame: u64 = 0;
     var last_frame: u64 = 0;
 
+    // While we're still rendering...
     while (running) {
+        // Calculate the delta time
         last_frame = current_frame;
         current_frame = renderer.now();
         var delta = renderer.getDeltaTime(current_frame, last_frame);
 
+        // Mouse state
         var is_mouse_down: bool = false;
 
+        // Handle events
         for (try renderer.events()) |event| {
             switch (event) {
                 .quit => running = false,
