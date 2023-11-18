ee3724ab83a8483a0768d3a163cb5cc970f95a1a
Zack Kollar <rallokkcaz@gmail.com>
2023-11-18 00:57:21 -0500

    :hammer: Fix missing default scope for coloredLogFn

--- src/main.zig
+++ src/main.zig
@@ -146,7 +146,7 @@ pub fn coloredLogFn(
     comptime format: []const u8,
     args: anytype,
 ) void {
-    const scope_prefix = "(" ++ @tagName(scope) ++ "): ";
+    const scope_prefix = if (scope == .default) ": " else "(" ++ @tagName(scope) ++ "): ";
     const prefix = "[" ++ comptime coloredLogLevel(level) ++ "] " ++ scope_prefix;
 
     // Print the message to stderr, silently ignoring any errors
